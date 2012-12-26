d5b45b3f3d150807ecd262ba21fa25dc8db8f70a
nstungxd@gmail.com <nstungxd@gmail.com@331cc575-f7f5-ba17-dbee-8a46313d160b>
2012-12-26 08:06:46 +0000

    Create project

---
+++ trunk/IPS.Data.SqlCe/Repositories/UserRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IPS.Model;
+
+namespace IPS.Data.SqlCe.Repositories
+{
+    public class UserRepository
+    {
+        public IEnumerable<User> GetAll()
+        {
+            List<User> listUser = new List<User>();
+            return listUser;
+        }
+    }
+}
---
+++ trunk/IPS.Domain/Handlers/UpdateUserInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPS.Domain.Handlers
+{
+    class UpdateUserInfo
+    {
+        public int UpdateUserStatus(int userId, int status)
+        {
+            int result = -1;
+            /* execute at here(for example) */
+            return result;
+        }
+    }
+}
