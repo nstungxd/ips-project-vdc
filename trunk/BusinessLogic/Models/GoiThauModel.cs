379c9b9cfc7570181961c315c97521881b7acb84
nvhoa.itviet@gmail.com <nvhoa.itviet@gmail.com@331cc575-f7f5-ba17-dbee-8a46313d160b>
2013-01-07 02:28:34 +0000

    change return type of list DanhSachGoiThau

--- trunk/BusinessLogic/Models/GoiThauModel.cs
+++ trunk/BusinessLogic/Models/GoiThauModel.cs
@@ -7,7 +7,49 @@ using System.Web;
 namespace BusinessLogic.Models
 {
     [DataContract]
-    public class GoiThauModel
+    public class GoiThauShortModel
     {
+        [DataMember]
+        public string MaDonVi { get; set; }
+
+        [DataMember]
+        public long IdDuAn { get; set; }
+
+        [DataMember]
+        public long IdGoiThau { get; set; }
+
+        [DataMember]
+        public string HinhThucDauThau { get; set; }
+
+        [DataMember]
+        public string TrangThaiThucHien { get; set; }
+
+        [DataMember]
+        public int TinhTrangXoa { get; set; }
+
+        [DataMember]
+        public long IdGiamSat { get; set; }
+
+        [DataMember]
+        public int GiaiDoanDauThau { get; set; }       
+
+        [DataMember]
+        public int KetQuaGiamSat { get; set; }
+
+        [DataMember]
+        public string GhiChuGiamSat { get; set; }
+    }
+
+    [DataContract]
+    public class ListGoiThauModelGridView
+    {
+        [DataMember]
+        public List<GoiThauShortModel> GoiThauModelsGridView { get; set; }
+
+        [DataMember]
+        public long TotalRecords { get; set; }
+
+        [DataMember]
+        public long TotalPage { get; set; }
     }
 }
